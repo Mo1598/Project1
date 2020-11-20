74bd4b8e6d4567f86cc27d50a080436d85e9bbe0
Mo1598 <ngugipious1598@gmail.com>
2020-11-20 12:11:46 +0300

    Hearder section completed

--- index.view.php
+++ index.view.php
@@ -1,96 +1,144 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/7ad1f18ede.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Style.css">
 
     <title>Hello, world!</title>
-  </head>
-  <body>
+</head>
 
-  <!-- navbar section -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">UpWork</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    </form>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          FIND WORK
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Find Work</a>
-          <a class="dropdown-item" href="#">Saved Jobs</a>
-          <a class="dropdown-item" href="#">Proposals </a>
-          <a class="dropdown-item" href="#">Profile </a>
-          <a class="dropdown-item" href="#">My Stats</a>
-          <a class="dropdown-item" href="#">UpWork Readiness Test</a>
-          <a class="dropdown-item" href="#">My Projects Dashboard</a>
-          <!-- <div class="dropdown-divider"></div> -->
+<body>
+
+    <!-- navbar section -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+
+            <a class="navbar-brand" href="#" style="font-weight:bold;">UpWork</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto" style="">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            FIND WORK
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Find Work</a>
+                            <a class="dropdown-item" href="#">Saved Jobs</a>
+                            <a class="dropdown-item" href="#">Proposals </a>
+                            <a class="dropdown-item" href="#">Profile </a>
+                            <a class="dropdown-item" href="#">My Stats</a>
+                            <a class="dropdown-item" href="#">UpWork Readiness Test</a>
+                            <a class="dropdown-item" href="#">My Projects Dashboard</a>
+                            <!-- <div class="dropdown-divider"></div> -->
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            MY JOBS
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">My Jobs</a>
+                            <a class="dropdown-item" href="#">All Contracts</a>
+                            <a class="dropdown-item" href="#">Work Diary </a>
+                            <!-- <div class="dropdown-divider"></div> -->
+                        </div>
+                    </li>
+                    <!-- <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li> -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            REPORTS
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Overview</a>
+                            <a class="dropdown-item" href="#">My Reports</a>
+                            <a class="dropdown-item" href="#">Lifetime Billings by Client</a>
+                            <a class="dropdown-item" href="#">Connects History</a>
+                            <a class="dropdown-item" href="#">Transactions History</a>
+                            <a class="dropdown-item" href="#">Certificate of Earnings</a>
+
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#" tabindex="-1">MESSAGES</a>
+                    </li>
+                    <li class="nav-item">
+                        <i class="fas fa-question" style="padding-top:0.6rem; margin-left:2rem; font-size:24px;"></i>
+                    </li>
+                    <li class="nav-item">
+                        <i class="far fa-bell" style="padding-top:0.6rem; margin-left:2rem; font-size:24px;"></i>
+                    </li>
+                    <li class="nav-item">
+                        <i class="fab fa-telegram-plane"
+                            style="padding-top:0.6rem; margin-left:2rem; font-size:24px;"></i>
+                    </li>
+                    <li class="nav-item">
+                        <img src="img/profpic.png" alt="" srcset="" class="profile-image">
+                    </li>
+                </ul>
+
+            </div>
+
         </div>
-      </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          MY JOBS
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">My Jobs</a>
-          <a class="dropdown-item" href="#">All Contracts</a>
-          <a class="dropdown-item" href="#">Work Diary </a>
-          <!-- <div class="dropdown-divider"></div> -->
+
+    </nav>
+
+
+    <!-- end of navbar section -->
+
+    <!-- content preview -->
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <h1 class="m-0-top">Find Work</h1>
+            </div>
+            <div class="col-md-6">
+                <form class="form-inline my-2 my-lg-0">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Find Jobs"
+                            aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary" type="button">Search</button>
+                        </div>
+                    </div>
+                </form>
+                <a href="">Advanced search</a>
+            </div>
+            <div class="col-md-3">
+                here
+            </div>
         </div>
-      </li>
-      <!-- <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li> -->
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          REPORTS
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Overview</a>
-          <a class="dropdown-item" href="#">My Reports</a>
-          <a class="dropdown-item" href="#">Lifetime Billings by Client</a>
-          <a class="dropdown-item" href="#">Connects History</a>
-          <a class="dropdown-item" href="#">Transactions History</a>
-          <a class="dropdown-item" href="#">Certificate of Earnings</a>
-          
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#" tabindex="-1" >MESSAGES</a>
-      </li>
-      <li class="nav-item">
-      <i class="fas fa-question" style="padding-top:0.6rem; margin-left:2rem"></i>
-      </li>
-      <li class="nav-item">
-      <i class="far fa-bell" style="padding-top:0.6rem; margin-left:2rem"></i>      
-      </li>
-      <li class="nav-item">
-      <i class="fab fa-telegram-plane" style="padding-top:0.6rem; margin-left:2rem"></i>      
-      </li>
-      <li class="nav-item">
-      <i class="far fa-user-circle" style="padding-top:0.6rem; margin-left:2rem"></i>      </li>
-    </ul>
-    
-  </div>
-</nav>
-<!-- end of navbar section -->
+    </div>
+
+    <!-- end of content preview -->
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -98,5 +146,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     -->
 
-  </body>
+</body>
+
 </html>
